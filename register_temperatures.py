13d6e93c85e185f5dd0b97478b327b5390a5572a
mwerlen <maxime@werlen.fr>
2020-11-20 21:25:55 +0100

    First POC

---
+++ register_temperatures.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+#import sys
+import subprocess
+import time
+import threading
+import queue
+import json
+#import mysql.connector
+#from mysql.connector import errorcode
+
+# Forked from : https://github.com/jcarduino/rtl_433_2db
+
+# Please create a mysql database for user rtl433db with create rights so table can be created
+# change ip for database server
+# install mysql connector
+# install phython 2.7
+# let it run ;)
+
+config = {
+  'user': 'rtl433db',
+  'password': 'fWMqwmFNKbK9upjT',
+  'host': '192.168.0.8',
+  'database': 'rtl433db',
+  'raise_on_warnings': True,
+}
+
+process = None
+stdout_queue = queue.Queue()
+stdout_reader = None
+stderr_queue = queue.Queue()
+stderr_reader = None
+
+class AsynchronousFileReader(threading.Thread):
+    '''
+    Helper class to implement asynchronous reading of a file
+    in a separate thread. Pushes read lines on a queue to
+    be consumed in another thread.
+    '''
+
+    def __init__(self, fd, myqueue):
+        assert isinstance(myqueue, queue.Queue)
+        assert callable(fd.readline)
+        threading.Thread.__init__(self)
+        self._fd = fd
+        self._queue = myqueue
+
+    def run(self):
+        '''The body of the tread: read lines and put them on the queue.'''
+        for line in iter(self._fd.readline, ''):
+            self._queue.put(line)
+
+    def eof(self):
+        '''Check whether there is no more content to expect.'''
+        return not self.is_alive() and self._queue.empty()
+
+def start_subprocess(args):
+    '''
+    Example of how to consume standard output and standard error of
+    a subprocess asynchronously without risk on deadlocking.
+    '''
+    print("\n\nStarting sub process " + ' '.join(args) + "\n")
+
+
+    # Launch the command as subprocess.
+    global process
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Launch the asynchronous readers of the process' stdout and stderr.
+    global stdout_queue
+    global stdout_reader
+    global stderr_queue
+    global stderr_reader
+    stdout_reader = AsynchronousFileReader(process.stdout, stdout_queue)
+    stdout_reader.start()
+    stderr_reader = AsynchronousFileReader(process.stderr, stderr_queue)
+    stderr_reader.start()
+    
+
+#def connect_db():
+#    # do database stuff init
+#    try:
+#        print("Connecting to database")
+#        cnx = mysql.connector.connect(**config)
+#    except mysql.connector.Error as err:
+#        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#            print("Something is wrong with your user name or password")
+#        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#            print("Database does not exists, please create it before using this script.")
+#            print("Tables can be created by the script.")
+#        else:
+#            print(err)
+#    reconnectdb=0#if 0 then no error or need ro be reconnected
+#    #else:
+#    #cnx.close()
+#    cursor = cnx.cursor()
+#    TABLES = {}
+#    TABLES['SensorData'] = (
+#        "CREATE TABLE `SensorData` ("
+#        "  `sensor_id` INT UNSIGNED NOT NULL,"
+#        "  `whatdata` varchar(50) NOT NULL,"
+#        "  `data` float NOT NULL,"
+#        "  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+#        ") ENGINE =InnoDB DEFAULT CHARSET=latin1")
+#    for name, ddl in TABLES.iteritems():
+#        try:
+#            print("Checking table {}: ".format(name))
+#            cursor.execute(ddl)
+#        except mysql.connector.Error as err:
+#            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+#                print("Table seams to exist, no need to create it.")
+#            else:
+#                print(err.msg)
+#        else:
+#            print("OK")
+#    add_sensordata= ("INSERT INTO SensorData "
+#                     "(sensor_id, whatdata, data) "
+#                     "VALUES (%s, %s, %s)")
+
+
+def sanitize(text):
+    return text.replace(" ", "_")
+
+
+def process_inputs():
+    # do queue loop, entering data to database
+    # Check the queues if we received some output (until there is nothing more to get).
+    while not stdout_reader.eof() or not stderr_reader.eof():
+        # Show what we received from err output.
+        while not stderr_queue.empty():
+            line = stderr_queue.get().decode('UTF-8')
+            print(line.rstrip())
+
+        while not stdout_queue.empty():
+            line = stdout_queue.get()
+            data = json.loads(line)
+            # {
+            #   "time" : "2020-11-20 20:06:45", 
+            #   "brand" : "LaCrosse", 
+            #   "model" : "LaCrosse-TX29IT", 
+            #   "id" : 7, 
+            #   "battery_ok" : 1, 
+            #   "newbattery" : 0, 
+            #   "temperature_C" : 4.000, 
+            #   "mic" : "CRC"
+            # }
+            when = int(time.time())
+            label = sanitize(data["model"])
+
+            if "channel" in data:
+                label += ".CH=" + str(data["channel"])
+            elif "id" in data:
+                label += ".ID=" + str(data["id"])
+
+            if "battery_ok" in data:
+                if data["battery_ok"] == 0:
+                    print(f'⚠ {label} Battery empty!')
+
+            if "temperature_C" in data:
+                print(f'Received from {label} : Temperature {data["temperature_C"]}')
+
+            if "humidity" in data:
+                print(label + ' Humidity ', data["humidity"])
+
+            #######################
+            #last field, put in db
+            # UPDATE DB
+            #########################
+           # try:
+           #     if reconnectdb:
+           #         print("Trying reconnecting to database")
+           #      #   cnx.reconnect()
+           #         reconnectdb=0
+           #     print("Kaku ID "+str(device)+" Unit "+unit+" Grp"+group+" Do "+command+" Dim "+dim)
+           #     sensordata = (device,'Kaku '+unit+' Grp'+group+" Do "+command+ ' Dim '+ dim,dimvalue)
+           #     #cursor.execute(add_sensordata,sensordata)
+           #     # Make sure data is committed to the database
+           #     print("committing")
+           #     #cnx.commit()
+           # except mysql.connector.Error as err:
+           #     if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+           #         print("Table seams to exist, no need to create it.")
+           #     else:
+           #         print(err.msg)
+           #     reconnectdb=1
+           #     print("Error connecting to database")
+        
+        # Sleep a bit before asking the readers again.
+        time.sleep(.1)
+
+
+def close_all():
+    # Let's be tidy and join the threads we've started.
+   # try:
+   #     cursor.close()
+   #     cnx.close()
+   # except:
+   #     pass
+    stdout_reader.join()
+    stderr_reader.join()
+
+    # Close subprocess' file descriptors.
+    process.stdout.close()
+    process.stderr.close()
+
+
+if __name__ == '__main__':
+    # The main flow:
+        #check if database is present, create tablesif no tables present
+
+
+    start_subprocess(["/usr/local/bin/rtl_433", "-C", "si", "-f", "868M", "-F", "json", "-M", "utc", "-R76"])
+    #connect_db()
+    process_inputs()
+    close_all()
+    print("Closing down")
+
